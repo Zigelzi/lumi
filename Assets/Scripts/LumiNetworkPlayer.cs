d875b81c151e68cce2b47a293f1e968f3055001a
Miika Savela <miika.a.savela@gmail.com>
2021-12-29 17:08:50 +0200

    Initial commit with default scenes and network objects

---
+++ Assets/Scripts/LumiNetworkPlayer.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+public class LumiNetworkPlayer : NetworkBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ README.md
@@ -0,0 +1,6 @@
+# Lumi
+Multiplayer arena game made with Unity and Mirror.
+
+## Attribution
+Main menu image
+https://www.artstation.com/artwork/lLdqY
