e1071c782bccdf408aec19b7f77994e22194728b
Miika Savela <miika.a.savela@gmail.com>
2022-01-01 15:16:04 +0200

    Add player facing towards the mouse position and physics material for player

---
+++ Assets/Scripts/Player/PlayerTargeting.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using Mirror;
+
+public class PlayerTargeting : NetworkBehaviour
+{
+    [SerializeField] LayerMask groundLayer;
+    Camera mainCamera;
+    Mouse mouse;
+
+    void Start()
+    {
+        mainCamera = Camera.main;
+        mouse = Mouse.current;
+    }
+
+    void Update()
+    {
+        FaceAtMousePosition();
+    }
+
+    void FaceAtMousePosition()
+    {
+        RaycastHit hit;
+        Ray ray = mainCamera.ScreenPointToRay(mouse.position.ReadValue());
+
+        if (Physics.Raycast(ray, out hit, Mathf.Infinity, groundLayer))
+        {
+            transform.LookAt(hit.point);
+        }
+    }
+}
--- README.md
+++ README.md
@@ -3,12 +3,12 @@ Multiplayer arena game made with Unity and Mirror.
 
 ## Todo
 ### Core gameplay
-- [ ] Movement only in game arena
+- [x] Movement only in game arena
 - [ ] Firing with projectiles
 - [ ] Health
 - [ ] Win condition (Defeat enemy)
 - [ ] Camera positioning
-- [ ] Rotating player towards the mouse
+- [x] Rotating player towards the mouse
 
 ## Attribution
 Main menu image
